9f6303c167a2fdfaf7d4c091a3551ff6b1f00922
iamverysimp1e <ngyn.tuenhan@protonmail.com>
2023-02-21 22:13:52 -0600

    Updated

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { imagetools } from 'vite-imagetools'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [svelte(), imagetools()]
-})
